95166b2b0cbd2c3ef2f6aa203f3a05b4434a5113
nina <ninagbs@abv.bg>
2023-11-29 14:41:35 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListingComponent } from './listing/listing.component';
+import { CreateinvoiceComponent } from './createinvoice/createinvoice.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { RatingComponent } from './rating/rating.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer'
+import {DataTablesModule} from 'angular-datatables'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListingComponent,
+    CreateinvoiceComponent,
+    RatingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    NgbModule,
+    NgxExtendedPdfViewerModule,
+    DataTablesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
